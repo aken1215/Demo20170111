adfd5c6bb68c322d5ac0049750507517370fa621
Hank <aken1215@hotmail.com>
2017-01-10 15:59:18 +0800

    新增HR030 ViewModel

--- Demo/Scripts/TypeLite.Net4.d.ts
+++ Demo/Scripts/TypeLite.Net4.d.ts
@@ -7,5 +7,10 @@
 
 /// <reference path="Enums.ts" />
 
+declare module Hank.ViewModel {
+	interface HR030 {
+		EMPNO: string;
+	}
+}
 
 
---
+++ ViewModel/HR030.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hank.ViewModel
+{
+    public class HR030
+    {
+        public string EMPNO { get; set; }
+    }
+}
