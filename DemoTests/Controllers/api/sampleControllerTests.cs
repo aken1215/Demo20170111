7b8b0429415d789bdc3a13a9f75b4b15e9d229a5
Hank <aken1215@hotmail.com>
2017-01-10 17:36:22 +0800

    增加Test

---
+++ DemoTests/Controllers/api/sampleControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo.Controllers.api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers.api.Tests
+{
+    [TestClass()]
+    public class sampleControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var expected = "12345";
+
+            var actual = "12345";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
