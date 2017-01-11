4dce432862e099dbf3154be7160e420c5345e8a7
Hank <aken1215@hotmail.com>
2017-01-11 10:48:37 +0800

    新增Dapper的功能

---
+++ Demo/Models/AA001.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class AA001
+    {
+        public string EMPNO { get; set; }
+    }
+}
--- Demo/Repositories/sample/SampleRepository2.cs
+++ Demo/Repositories/sample/SampleRepository2.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Hank.ViewModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+using Demo.Models;
 
 namespace Demo.Repositories.sample
 {
     public class SampleRepository2 : ISampleRepository
     {
+        private string connString = ConfigurationManager.ConnectionStrings["ConnectString"].ToString();
         public HR030 GetHR030()
         {
-            return new HR030 { EMPNO = "00029087" };
+            IDbConnection conn = new SqlConnection(connString);
+            var aa001= conn.Query<AA001>("Select top 1 EMPNO from aa001").FirstOrDefault();
+
+            return new HR030 { EMPNO = aa001.EMPNO };
         }
     }
 }
